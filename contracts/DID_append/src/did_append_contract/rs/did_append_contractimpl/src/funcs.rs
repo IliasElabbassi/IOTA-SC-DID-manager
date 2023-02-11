12019d652aac50b6e472d168778c1de07adef9ce
Ilias El Abbassi <ilias.elabbassi.etu@univ-lille.fr>
2023-02-11 16:34:47 +0100

    fix: fixed a bit fo the add did function

--- contracts/DID_append/src/did_append_contract/rs/did_append_contractimpl/src/funcs.rs
+++ contracts/DID_append/src/did_append_contract/rs/did_append_contractimpl/src/funcs.rs
@@ -6,26 +6,26 @@ use wasmlib::*;
 
 use crate::*;
 
-// This was the only function i needed to change after generating the code
-pub fn func_add_did(ctx: &ScFuncContext, f: &AddDidContext) {
+// This was the only function I needed to change after generating the code
+pub fn func_add_did(_ctx: &ScFuncContext, f: &AddDidContext) {
     if f.params.new_did().exists() {
-        f.state.d_id().append(&f.params.new_did().value());
-        return;
+        // the DID aray is implemented has d_id in the genrated code
+        f.state.d_id().append_string(f.params.new_did().value());
     }
 }
 
-pub fn func_init(ctx: &ScFuncContext, f: &InitContext) {
+pub fn func_init(_ctx: &ScFuncContext, f: &InitContext) {
     if f.params.owner().exists() {
         f.state.owner().set_value(&f.params.owner().value());
         return;
     }
-    f.state.owner().set_value(&ctx.request_sender());
+    f.state.owner().set_value(&_ctx.request_sender());
 }
 
-pub fn func_set_owner(ctx: &ScFuncContext, f: &SetOwnerContext) {
+pub fn func_set_owner(_ctx: &ScFuncContext, f: &SetOwnerContext) {
     f.state.owner().set_value(&f.params.owner().value());
 }
 
-pub fn view_get_owner(ctx: &ScViewContext, f: &GetOwnerContext) {
+pub fn view_get_owner(_ctx: &ScViewContext, f: &GetOwnerContext) {
     f.results.owner().set_value(&f.state.owner().value());
 }
