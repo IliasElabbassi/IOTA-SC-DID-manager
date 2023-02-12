ba607697f871e60781d532c4bb4d4f27c5e97baa
Ilias El Abbassi <ilias.elabbassi.etu@univ-lille.fr>
2023-02-12 16:58:40 +0100

    fix: appending string to the state array finally done

--- contracts/DID_append/src/did_append_contract/rs/did_append_contractimpl/src/funcs.rs
+++ contracts/DID_append/src/did_append_contract/rs/did_append_contractimpl/src/funcs.rs
@@ -9,8 +9,22 @@ use crate::*;
 // This was the only function I needed to change after generating the code
 pub fn func_add_did(_ctx: &ScFuncContext, f: &AddDidContext) {
     if f.params.new_did().exists() {
+        // We get the DID parameters provided to the function call
+        let did : ScMutableString = f.params.new_did.value();
+
         // the DID aray is implemented has d_id in the genrated code
-        f.state.d_id().append_string(f.params.new_did().value());
+        // Get the proxy to the 'DID' array in the state storage.
+        let did_list: ArrayOfMutableString = f.state.d_id();
+        // f.state.d_id().append_string(f.params.new_did().value());
+
+        // Now we will append the new did to the did_list array.
+        // We create an ScMutableString proxy to a string value that lives
+        // at the end of the did_list array (no value yet, since we're appending).
+        let new_did: ScMutableString = member_list.append_string();
+
+        // And finally we append the new string to the array by telling the proxy
+        // to update the value it refers to with the 'did' parameter.
+        new_did.set_value(&did);
     }
 }
 
