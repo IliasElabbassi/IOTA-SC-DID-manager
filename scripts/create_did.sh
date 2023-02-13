60e6fe66523c26ffd568b973722d614a4f112b9f
Ilias El Abbassi <ilias.elabbassi.etu@univ-lille.fr>
2023-02-13 14:25:00 +0100

    feat: rdv

--- scripts/create_did.sh
+++ scripts/create_did.sh
@@ -1,9 +1,12 @@
 // first represent the options
 // 1 - create a did and push it to ISC
-// 2 - ...
-
+// 2 - update a did
+// 3 - delete a did
+// 4 - read a did
 
 // 1 - create a did and push it to ISC
 // cmd to create a did
 // cmd to push the newly did to the ISC
 
+did = ./did_src
+wasp-cli chain post-request appenddid addDid String newDID did
