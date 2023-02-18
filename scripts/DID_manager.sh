6d79be211d2da2cd72cde75d13c9fdfc7b4885d0
Ilias El Abbassi <iliaselabbassi@outlook.fr>
2023-02-18 21:48:58 +0100

    feat: DID manger bash

---
+++ scripts/DID_manager.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+IFS=' '
+ISCname='realdid'
+stronHoldFile="./did-strong.hodl"
+
+echo "Please choose what to do :"
+echo "[1] create did and push it to the ISC"
+echo "[2] update a did in the ISC"
+echo "[3] delete a did in the ISC"
+echo "[4] view dids in the ISC"
+echo "[5] get the did at a specific index"
+
+read which
+
+if [ $which -eq 1 ]; then
+    echo "Creating a DID :"
+    echo "Please enter a password for the DID : "
+    read pass
+    if test -f "$stronHoldFile"; then
+        echo "Removing old did-strong.hodl file"
+        rm ./did-strong.hodl
+    fi
+    echo "Generating DID..."
+    res=$( ../did_src/target/debug/did_src $pass )
+    read -ra arr <<< $res
+    DID=${arr[0]}
+    echo "Generated DID : $DID"
+    echo "Uploading DID to ISC..."
+    wasp-cli chain post-request $ISCname addDid string newDID string $DID
+fi
+
+if [ $which -eq 2 ]; then
+    echo "Updating a DID :"
+    echo "Please enter the DID to update : "
+    read toUpdate
+    echo "Please enter the value of the new DID : "
+    read value
+    echo "Updating the DID..."
+    wasp-cli chain post-request $ISCname updateDid string toUpdateDID string $toUpdate string value string $value 
+fi
+
+if [ $which -eq 3 ]; then
+    echo "Deleting a DID :"
+    echo "Please enter the DID to delete : "
+    read toDelete
+    echo "Updating the DID..."
+    wasp-cli chain post-request $ISCname deleteDid string toDeleteDID string $toDelete
+fi
+
+if [ $which -eq 4 ]; then
+    echo "View DIDs :"
+    didLen=$( wasp-cli chain call-view $ISCname getLength | wasp-cli decode int8 length int8 )
+
+    for didIndex in {0..$didLen} do
+        wasp-cli chain call-view $ISCname getDID int8 index int8 $didIndex | wasp-cli decode string indexedDID string
+    done
+fi
+
+if [ $which -eq 5 ]; then
+    echo "View a DID :"
+    echo "Please enter the index of the did you want to view :"
+    read indexDID
+    echo "Reading the DID..."
+    wasp-cli chain call-view $ISCname getDID int8 index int8 $indexDID | wasp-cli decode string indexedDID string
+fi
